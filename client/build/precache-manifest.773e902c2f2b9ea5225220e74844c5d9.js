56a000808a06730e19eccb7cc294a7608411a373
Miranda Jaroneski <miranda.jaroneski@gmail.com>
2019-12-19 21:12:34 -0500

    first

---
+++ client/build/precache-manifest.773e902c2f2b9ea5225220e74844c5d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "849eacd749ebcbf60790",
+    "url": "/static/js/main.d7ef874a.chunk.js"
+  },
+  {
+    "revision": "f2179e7f22e6577b35cf",
+    "url": "/static/js/2.565b7cae.chunk.js"
+  },
+  {
+    "revision": "849eacd749ebcbf60790",
+    "url": "/static/css/main.c594c1a3.chunk.css"
+  },
+  {
+    "revision": "dd5d76140eaa68244f2bef267b1e619e",
+    "url": "/index.html"
+  }
+];
